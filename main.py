8346b679aca7b83ec1abcca478c503f33d322f95
Shoma FUKUDA <fkshom@gmail.com>
2021-01-27 01:02:02 +0900

    wip

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
+
+app = CustomFlask(__name__,
+            static_folder = "./dist/static",
+            template_folder = "./dist")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
