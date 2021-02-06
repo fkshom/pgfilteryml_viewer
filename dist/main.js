872d401bb2bbdea0de0a02e9d09d45ad4f14fa8b
FUKUDA Shoma <fkshom@gmail.com>
2021-02-06 16:59:39 +0900

    wip

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,9 @@
 var app = new Vue({
     el: "#app",
     data:{
-      datetime: "",
+      datetime: "02/15（日）13:00-15:00",
       workcontent: "",
-      worker: "",
-      place: "",
+      worker: "田中、鈴木",
+      place: "事務所2",
     }
   })
